3a2f312017dc73335498a5b1b068a5a941ab3336
malboushi <mohammad.alaa.love.619@gmail.com>
2026-08-05 16:26:46 +0300

    Delete .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const {join} = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // إجبار النظام على حفظ المتصفح داخل مجلد المشروع
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
